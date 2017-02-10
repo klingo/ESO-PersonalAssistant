c003170e278cb2d2f757192167a7ac0067dbc2b9
klingo <klingo@klingonium.com>
2017-02-10 21:07:08 +0100

    dirty-fix of a bug (temporary)

--- libs/Utilities/EventDispatcher.lua
+++ libs/Utilities/EventDispatcher.lua
@@ -9,9 +9,13 @@ PAED = {}
 
 --  Acts as a dispatcher between PARepair and PAJunk that both depend on [EVENT_OPEN_STORE]
 function PAED.EventOpenStore()
+    -- to avoid a bug, call PARepair first, although in some cases this can be inefficient
+    -- the problem is that the stats of sellink junk is influenced by the repair bill, resulting in negative numbers
+    PAR.OnShopOpen()
+
     -- first execute PAJunk (to sell junk and get gold)
     PAJ.OnShopOpen()
 
     -- only then execute PARepair (to spend gold for repairs)
-    PAR.OnShopOpen()
+    -- PAR.OnShopOpen()
 end
