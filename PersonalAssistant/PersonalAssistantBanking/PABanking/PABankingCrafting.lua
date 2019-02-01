81295b594b9c8e401f16fb03dfc55674b436b75c
Fabian Schär <klingo@klingonium.com>
2019-02-01 22:32:00 +0100

    technical DRAFT implementation for new banking functions

--- PersonalAssistant/PersonalAssistantBanking/PABanking/PABankingCrafting.lua
+++ PersonalAssistant/PersonalAssistantBanking/PABanking/PABankingCrafting.lua
@@ -7,36 +7,21 @@ local L = PA.Localization
 
 -- ---------------------------------------------------------------------------------------------------------------------
 
-local doneOnce = false
+-- TODO: Assumption: Filling up existing stacks can be done immediately; creating new stacks takes time (i.e. zo_callLater needed)
+-- TODO: This is confirmed!
 
+local isBankTransferBlocked = false
 
-local function GetItemDataFilterComparator()
-    return function(itemData)
---        if not doneOnce then
---            doneOnce = true
---            d(itemData)
---        end
---
---        itemData.bagId
---        itemData.slotIndex
---    itemData.specializedItemType
---    itemData.itemType
---    itemData.stolen
---    itemData.isJunk
---    itemData.filterType -- 23
+local depositComparator
+local withdrawComparator
 
---        d(itemData.itemType)
+local toDepositBagCache
+local toWithdrawBagCache
 
---        local itemFilterType = GetItemFilterTypeInfo(itemData.bagId, itemData.slotIndex)
---        d(itemFilterType)
+local toDepositBagIndex
+local toWithdragBagIndex
 
---        return ZO_InventoryUtils_DoesNewItemMatchFilterType(itemData, ITEMFILTERTYPE_WOODWORKING)
-
-        return itemData.itemType == ITEMTYPE_WOODWORKING_BOOSTER or itemData.itemType == ITEMTYPE_WOODWORKING_MATERIAL or itemData.itemType == ITEMTYPE_WOODWORKING_RAW_MATERIAL
-    end
-
-end
-
+local TIMER_TO_BE_REPLACED = 200
 
 
 local function getComparator(itemTypeList)
@@ -49,7 +34,7 @@ local function getComparator(itemTypeList)
 end
 
 
-local function moveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
+local function requestMoveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
     if IsProtectedFunction("RequestMoveItem") then
         CallSecureProtected("RequestMoveItem", sourceBag, sourceSlot, destBag, destSlot, stackCount)
     else
@@ -58,12 +43,68 @@ local function moveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
 end
 
 
+local function findFirstEmptySlotAndTargetBagFromSourceBag(sourceBagId)
+    local targetBagId
+    local targetSlotIndex
+
+    -- try to find an empty slot in the target bag; based on the provided sourceBag
+    if sourceBagId == BAG_BACKPACK then
+        targetBagId = BAG_BANK
+        targetSlotIndex = FindFirstEmptySlotInBag(targetBagId)
+        if targetSlotIndex == nil then
+            targetBagId = BAG_BAG_SUBSCRIBER_BANK
+            targetSlotIndex = FindFirstEmptySlotInBag(targetBagId)
+        end
+    elseif sourceBagId == BAG_BANK or sourceBagId == BAG_BAG_SUBSCRIBER_BANK then
+        targetBagId = BAG_BACKPACK
+        targetSlotIndex = FindFirstEmptySlotInBag(targetBagId)
+    end
+
+    -- check if both values could be defined
+    if targetBagId ~= nil and targetSlotIndex ~= nil then
+        -- yes; valid empty sloud found; return information
+        return targetBagId, targetSlotIndex
+    end
+    -- no; not enough space or bag not found; return nil, nil
+    return nil, nil
+end
+
+
+local function moveSecureItemsFromTo(notMovedTable, startIndex)
+    local fromBagItemData = notMovedTable[startIndex]
+    local targetBagId, firstEmptySlot = findFirstEmptySlotAndTargetBagFromSourceBag(fromBagItemData.bagId)
+    if (targetBagId ~= nil and firstEmptySlot ~= nil) then
+        local sourceStack, _ = GetSlotStackSize(fromBagItemData.bagId, fromBagItemData.slotIndex)
+
+        d("2) move " .. sourceStack .. " x " .. fromBagItemData.name)
+        requestMoveItem(fromBagItemData.bagId, fromBagItemData.slotIndex, targetBagId, firstEmptySlot, sourceStack)
+
+        local newStartIndex = startIndex + 1
+        if newStartIndex <= #notMovedTable then
+            zo_callLater(function()
+                moveSecureItemsFromTo(notMovedTable, newStartIndex)
+            end, TIMER_TO_BE_REPLACED)
+            -- timer = timer + PASV.Banking[PA.activeProfile].depositTimerInterval
+            -- TODO: implement in UI+settings
+        else
+            d("2) all done!")
+        end
+    else
+        d("2) cannot move " .. fromBagItemData.name .. " - not enough space")
+        -- TODO: abort; dont continue
+    end
+
+end
+
+
 local function moveItemsFromTo(fromBagCache, toBagCache, newStacksAllowed)
 
-    local notMovedTable = setmetatable({}, { __index = table })
+--    local notMovedTable = setmetatable({}, { __index = table })
+    local notMovedTable = {}
 
     for _, fromBagItemData in pairs(fromBagCache) do
-        local itemMoved = false
+        local isItemMoved = false
+        local hasNoStacksLeft = false
 
         for _, toBagItemData in pairs(toBagCache) do
             if fromBagItemData.itemInstanceId == toBagItemData.itemInstanceId then
@@ -75,44 +116,39 @@ local function moveItemsFromTo(fromBagCache, toBagCache, newStacksAllowed)
                     local sourceStack, _ = GetSlotStackSize(fromBagItemData.bagId, fromBagItemData.slotIndex)
                     if sourceStack <= targetFreeStacks then
                         -- enough space to move all
-                        moveItem(fromBagItemData.bagId, fromBagItemData.slotIndex, toBagItemData.bagId, toBagItemData.slotIndex, sourceStack)
-                        itemMoved = true
-                        d("move " .. sourceStack .. " x " .. fromBagItemData.name)
+                        d("1) move " .. sourceStack .. " x " .. fromBagItemData.name)
+                        requestMoveItem(fromBagItemData.bagId, fromBagItemData.slotIndex, toBagItemData.bagId, toBagItemData.slotIndex, sourceStack)
+                        isItemMoved = true
+                        hasNoStacksLeft = true
                     else
                         -- not enough space, only fill up stack possible
-                        moveItem(fromBagItemData.bagId, fromBagItemData.slotIndex, toBagItemData.bagId, toBagItemData.slotIndex, targetFreeStacks)
-                        itemMoved = true
-                        d("move " .. targetFreeStacks .. "/" .. sourceStack .. " x " .. fromBagItemData.name)
-
-                        if newStacksAllowed then
-                            local firstEmptySlot = FindFirstEmptySlotInBag(toBagItemData.bagId)
-                            if (firstEmptySlot ~= nil) then
-                                local remainingStack = sourceStack - targetFreeStacks
-                                moveItem(fromBagItemData.bagId, fromBagItemData.slotIndex, toBagItemData.bagId, firstEmptySlot, remainingStack)
-                                d("move remaining " .. remainingStack .. "/" .. sourceStack .. " x " .. fromBagItemData.name)
-                            end
-                        end
+                        d("1) move " .. targetFreeStacks .. "/" .. sourceStack .. " x " .. fromBagItemData.name)
+                        requestMoveItem(fromBagItemData.bagId, fromBagItemData.slotIndex, toBagItemData.bagId, toBagItemData.slotIndex, targetFreeStacks)
+                        isItemMoved = true
                     end
                 end
             end
 
-            if itemMoved then break end
+            -- stop loop if item was already moved and no stacks to be moved are left
+            if isItemMoved and hasNoStacksLeft then break end
         end
 
-        if not itemMoved then
-            notMovedTable:insert(fromBagItemData)
+        -- if the item could not be moved (because no further existing stack to fill up), add it to the notMoved table
+        if not isItemMoved and newStacksAllowed then
+            d("1) add "..fromBagItemData.name.." to notMovedTable")
+--            notMovedTable:insert(fromBagItemData)
+            table.insert(notMovedTable, fromBagItemData)
         end
     end
 
-    for _, toBagItemData in pairs(notMovedTable) do
-        -- TODO: check if creating new stacks allowed
-        d(toBagItemData.name)
+    -- after initial run-through, go though all not yet moved items and look for free slots for them
+    if #notMovedTable > 0 then
+        moveSecureItemsFromTo(notMovedTable, 1)
+    else
+        -- all stacking done; and no further items to be moved
+        -- TODO: end message?
+        d("1) all done!")
     end
-
-
-
-
-    -- TODO: open questions: how to find the correct bagId when creating new stacks?
 end
 
 
@@ -153,7 +189,7 @@ local function depositOrWithdrawCraftingItems()
     depositCraftingItems(depositComparator, true)
 
     -- then withdraw
-    withdrawCraftingItems(withdrawComparator, false)
+--    withdrawCraftingItems(withdrawComparator, false)
 
     -- finally try to deposit again (in case withdrawal freed up some space)
     -- TODO: how to check if still needed?
