e3e4cea574b1a11bee9bc5fac29648bcb910858d
Fabian Schär <klingo@klingonium.com>
2019-04-02 22:31:25 +0200

    updated French translations from Takit

--- PersonalAssistant/PersonalAssistantRepair/localization/fr.lua
+++ PersonalAssistant/PersonalAssistantRepair/localization/fr.lua
@@ -7,14 +7,14 @@ SafeAddString(SI_PA_MENU_REPAIR_DESCRIPTION, "PARepair répare votre armure et r
 
 SafeAddString(SI_PA_MENU_REPAIR_ENABLE, table.concat({PAC.COLORS.LIGHT_BLUE, "Activer la réparation auto pour les objets équipés"}), 1)
 
-SafeAddString(SI_PA_MENU_REPAIR_GOLD_HEADER, table.concat({" ", PAC.ICONS.CURRENCY[CURT_MONEY].NORMAL, "  ", "Réparer avec ", GetCurrencyName(CURT_MONEY)}), 1)
-SafeAddString(SI_PA_MENU_REPAIR_GOLD_ENABLE, table.concat({"Réparer les objets équipés avec ", GetCurrencyName(CURT_MONEY), " ?"}), 1)
+SafeAddString(SI_PA_MENU_REPAIR_GOLD_HEADER, table.concat({" ", PAC.ICONS.CURRENCY[CURT_MONEY].NORMAL, "  ", "Réparer avec de l'", GetCurrencyName(CURT_MONEY)}), 1)
+SafeAddString(SI_PA_MENU_REPAIR_GOLD_ENABLE, table.concat({"Réparer les objets équipés avec de l'", GetCurrencyName(CURT_MONEY), " ?"}), 1)
 SafeAddString(SI_PA_MENU_REPAIR_GOLD_ENABLE_T, "En visitant un marchand, tous les objets équipés en dessous du seuil de durabilité défini seront automatiquement réparés", 1)
 SafeAddString(SI_PA_MENU_REPAIR_GOLD_DURABILITY, "Seuil de durabilité en %", 1)
 SafeAddString(SI_PA_MENU_REPAIR_GOLD_DURABILITY_T, "Réparer les objets équipés que s'ils sont en-dessous du seuil de durabilité", 1)
 
-SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_HEADER, table.concat({" ", PAC.ICONS.ITEMS.REPAIRKIT.NORMAL, "  ", "Réparer avec ", GetString(SI_PA_MENU_BANKING_INDIVIDUAL_REPAIRKIT)}), 1)
-SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_ENABLE, table.concat({"Réparer les objets équipés avec ", GetString(SI_PA_MENU_BANKING_INDIVIDUAL_REPAIRKIT), " ?"}), 1)
+SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_HEADER, table.concat({" ", PAC.ICONS.ITEMS.REPAIRKIT.NORMAL, "  ", "Réparer avec des ", GetString(SI_PA_MENU_BANKING_INDIVIDUAL_REPAIRKIT)}), 1)
+SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_ENABLE, table.concat({"Réparer les objets équipés avec des ", GetString(SI_PA_MENU_BANKING_INDIVIDUAL_REPAIRKIT), " ?"}), 1)
 SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_ENABLE_T, "Sur le champ de bataille, tous les objets équipés en dessous du seuil de durabilité défini seront automatiquement réparés", 1)
 SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_DURABILITY, "Seuil de durabilité en %", 1)
 SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_DURABILITY_T, "Réparer les objets équipés que s'ils sont en-dessous du seuil de durabilit", 1)
@@ -27,8 +27,8 @@ SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_LOW_KIT_WARNING_T, table.concat({"Affi
 SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_LOW_KIT_THRESHOLD, "Seuil de nécessaires de réparation", 1)
 SafeAddString(SI_PA_MENU_REPAIR_REPAIRKIT_LOW_KIT_THRESHOLD_T, table.concat({"Si la quantité restante de ", GetString(SI_PA_MENU_BANKING_INDIVIDUAL_REPAIRKIT), " est en dessous du seuil, un message sera affiché dans la fenêtre de chat"}), 1)
 
-SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_HEADER, table.concat({" ", PAC.ICONS.ITEMS.SOULGEM.NORMAL, "  ", "Recharger les armes avec ", zo_strformat(GetString("SI_PA_ITEMTYPE", ITEMTYPE_SOUL_GEM), 2)}), 1)
-SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_ENABLE, table.concat({"Recharger les armes équipées avec ", zo_strformat(GetString("SI_PA_ITEMTYPE", ITEMTYPE_SOUL_GEM), 2), " ?"}), 1)
+SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_HEADER, table.concat({" ", PAC.ICONS.ITEMS.SOULGEM.NORMAL, "  ", "Recharger les armes avec des ", zo_strformat(GetString("SI_PA_ITEMTYPE", ITEMTYPE_SOUL_GEM), 2)}), 1)
+SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_ENABLE, table.concat({"Recharger les armes équipées avec des ", zo_strformat(GetString("SI_PA_ITEMTYPE", ITEMTYPE_SOUL_GEM), 2), " ?"}), 1)
 SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_ENABLE_T, "Re-Charge les armes équipées quand elles sont complétement déchargées. Cela utilisera d'abord des pierres d'âmes, puis ensuite des pierres d'âmes à couronne.", 1)
 --SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_CHATMODE, "tbd", 1) -- TODO: not yet used ingame
 --SafeAddString(SI_PA_MENU_REPAIR_RECHARGE_CHATMODE_T, "tbd", 1) -- TODO: not yet used ingame
