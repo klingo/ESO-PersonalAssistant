e01ec88c7e617baad0c134608e5cf7668d0c6ac8
klingo <klingo@klingonium.com>
2019-04-23 22:21:03 +0200

    PA: Improved debugging (#71)
    * make registered eventNames more human-understandable
    
    * remove author-restriction for debug slashcommands again

--- PersonalAssistant/PersonalAssistant.lua
+++ PersonalAssistant/PersonalAssistant.lua
@@ -70,11 +70,11 @@ local function initAddon(_, addOnName)
     PA.activeProfile = PASavedVars.Profile.activeProfile
 
     -- register additional slash-commands (only for Addon author)
-    if GetUnitName("player") == PACAddon.AUTHOR then
+--    if GetUnitName("player") == PACAddon.AUTHOR then
         SLASH_COMMANDS["/padebugon"] = function() PA.toggleDebug(true) end
         SLASH_COMMANDS["/padebugoff"] = function() PA.toggleDebug(false) end
         SLASH_COMMANDS["/palistevents"] = function() PAEM.listAllEventsInSet() end
-    end
+--    end
 end
 
 
