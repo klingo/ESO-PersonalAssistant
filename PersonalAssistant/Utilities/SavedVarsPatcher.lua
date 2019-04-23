e58344c89e0506a477e5e03e534442405e8fa6ef
Fabian Schär <klingo@klingonium.com>
2019-04-23 21:11:55 +0200

    new author-only debug function add new SavedVarsPatcher revert PABanking-SavedVars version from 3 to 2

---
+++ PersonalAssistant/Utilities/SavedVarsPatcher.lua
@@ -0,0 +1,45 @@
+-- Local instances of Global tables --
+local PA = PersonalAssistant
+local PAC = PA.Constants
+local PACAddon = PAC.ADDON
+local PAHF = PA.HelperFunctions
+local PAEM = PA.EventManager
+
+-- ---------------------------------------------------------------------------------------------------------------------
+
+local function applyPatchIfNeeded()
+    -- first unregister the event again
+    PAEM.UnregisterForEvent(PACAddon.NAME_RAW.GENERAL, EVENT_PLAYER_ACTIVATED, "SavedVarsPatcher")
+
+    -- then get stored savedVarsVersion for comparison
+    local PASavedVars = PA.SavedVars
+    local prevStoredSavedVarsVersion = tonumber(PASavedVars.General.savedVarsVersion)
+
+    -- Upgrade to v2.0.3
+    if prevStoredSavedVarsVersion < 020003 then
+        PAHF.debuglnAuthor("START Upgrading SavedVarsVersion to: 020003")
+        for profileNo = 1, PAC.GENERAL.MAX_PROFILES do
+            -- 1) get rid of:   PABanking.transactionInterval
+            PASavedVars.Banking[profileNo].transactionInterval = nil
+            -- 2) get rid of:   PABanking.Crafting.TransactionSettings
+            PASavedVars.Banking[profileNo].Crafting.TransactionSettings = nil
+            -- 3) migrate:      PABanking.Crafting.ItemTypes
+            local oldItemTypes = PASavedVars.Banking[profileNo].Crafting.ItemTypes
+            for itemTypeId, moveConfig in pairs(oldItemTypes) do
+                if istable(moveConfig) then
+                    PASavedVars.Banking[profileNo].Crafting.ItemTypes[itemTypeId] = moveConfig.moveMode
+                end
+            end
+        end
+        PAHF.debuglnAuthor("FINISH Upgrading SavedVarsVersion to: 020003")
+    end
+
+    -- in the end, update the savedVarsVersion
+    PASavedVars.General.savedVarsVersion = PACAddon.VERSION_RAW
+end
+
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Export
+PA.SavedVarsPatcher = {
+    applyPatchIfNeeded = applyPatchIfNeeded
+}
