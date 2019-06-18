f1763f653340990cbfdeb7a1536c6b532d56dee0
Fabian Schär <klingo@klingonium.com>
2019-06-18 22:30:55 +0200

    PA: fixed an issue when context menu was not properly rendered

--- PersonalAssistant/Menu/ItemContextMenu.lua
+++ PersonalAssistant/Menu/ItemContextMenu.lua
@@ -21,66 +21,62 @@ local function _addDynamicContextMenuEntries(itemLink, bagId, slotIndex)
 
     -- Add PABanking context menu entries
     if PA.Banking and PA.Banking.SavedVars.Custom.customItemsEnabled then
-        zo_callLater(function()
-            -- first make some checks whether banking rules are even allowed for this item
-            if _isBankingRuleNotAllowed(itemLink, bagId, slotIndex) then return end
+        -- first make some checks whether banking rules are even allowed for this item
+        if _isBankingRuleNotAllowed(itemLink, bagId, slotIndex) then return end
 
-            local PABCustomItemIds = PA.Banking.SavedVars.Custom.ItemIds
-            local isRuleExisting = PAHF.isKeyInTable(PABCustomItemIds, itemId)
-            local entries = {
-                {
-                    label = GetString(SI_PA_SUBMENU_PAB_ADD_RULE),
-                    callback = function()
-                        PA.CustomDialogs.initPABAddCustomRuleUIDialog()
-                        PA.CustomDialogs.showPABAddCustomRuleUIDIalog(itemLink)
-                    end,
-                    disabled = function() return isRuleExisting end,
-                },
-                {
-                    label = GetString(SI_PA_SUBMENU_PAB_EDIT_RULE),
-                    callback = function()
-                        PA.CustomDialogs.initPABAddCustomRuleUIDialog()
-                        PA.CustomDialogs.showPABAddCustomRuleUIDIalog(itemLink, PABCustomItemIds[itemId])
-                    end,
-                    disabled = function() return not isRuleExisting end,
-                },
-                {
-                    label = GetString(SI_PA_SUBMENU_PAB_DELETE_RULE),
-                    callback = function()
-                        PA.CustomDialogs.initPABAddCustomRuleUIDialog()
-                        PA.CustomDialogs.deletePABCustomRule(itemLink)
-                    end,
-                    disabled = function() return not isRuleExisting end,
-                }
+        local PABCustomItemIds = PA.Banking.SavedVars.Custom.ItemIds
+        local isRuleExisting = PAHF.isKeyInTable(PABCustomItemIds, itemId)
+        local entries = {
+            {
+                label = GetString(SI_PA_SUBMENU_PAB_ADD_RULE),
+                callback = function()
+                    PA.CustomDialogs.initPABAddCustomRuleUIDialog()
+                    PA.CustomDialogs.showPABAddCustomRuleUIDIalog(itemLink)
+                end,
+                disabled = function() return isRuleExisting end,
+            },
+            {
+                label = GetString(SI_PA_SUBMENU_PAB_EDIT_RULE),
+                callback = function()
+                    PA.CustomDialogs.initPABAddCustomRuleUIDialog()
+                    PA.CustomDialogs.showPABAddCustomRuleUIDIalog(itemLink, PABCustomItemIds[itemId])
+                end,
+                disabled = function() return not isRuleExisting end,
+            },
+            {
+                label = GetString(SI_PA_SUBMENU_PAB_DELETE_RULE),
+                callback = function()
+                    PA.CustomDialogs.initPABAddCustomRuleUIDialog()
+                    PA.CustomDialogs.deletePABCustomRule(itemLink)
+                end,
+                disabled = function() return not isRuleExisting end,
             }
-            AddCustomSubMenuItem(GetString(SI_PA_SUBMENU_PAB), entries)
-        end, 50)
+        }
+        AddCustomSubMenuItem(GetString(SI_PA_SUBMENU_PAB), entries)
     end
 
     -- Add PAJunk context menu entries
     if PA.Junk and PA.Junk.SavedVars.Custom.customItemsEnabled then
-        zo_callLater(function()
-            local PAJCustomItemIds = PA.Junk.SavedVars.Custom.ItemIds
-            local canBeMarkedAsJunk = CanItemBeMarkedAsJunk(bagId, slotIndex)
-            local isRuleExisting = PAHF.isKeyInTable(PAJCustomItemIds, itemId)
-            local entries = {
-                {
-                    label = GetString(SI_PA_SUBMENU_PAJ_MARK_PERM_JUNK),
-                    callback = function()
-                        PA.Junk.addItemToPermanentJunk(itemLink, bagId, slotIndex)
-                    end,
-                    disabled = function() return not canBeMarkedAsJunk or isRuleExisting end,
-                },
-                {
-                    label = GetString(SI_PA_SUBMENU_PAJ_UNMARK_PERM_JUNK),
-                    callback = function()
-                        PA.Junk.removeItemFromPermanentJunk(itemLink)
-                    end,
-                    disabled = function() return not isRuleExisting end,
-                }
+        local PAJCustomItemIds = PA.Junk.SavedVars.Custom.ItemIds
+        local canBeMarkedAsJunk = CanItemBeMarkedAsJunk(bagId, slotIndex)
+        local isRuleExisting = PAHF.isKeyInTable(PAJCustomItemIds, itemId)
+        local entries = {
+            {
+                label = GetString(SI_PA_SUBMENU_PAJ_MARK_PERM_JUNK),
+                callback = function()
+                    PA.Junk.addItemToPermanentJunk(itemLink, bagId, slotIndex)
+                end,
+                disabled = function() return not canBeMarkedAsJunk or isRuleExisting end,
+            },
+            {
+                label = GetString(SI_PA_SUBMENU_PAJ_UNMARK_PERM_JUNK),
+                callback = function()
+                    PA.Junk.removeItemFromPermanentJunk(itemLink)
+                end,
+                disabled = function() return not isRuleExisting end,
             }
-            AddCustomSubMenuItem(GetString(SI_PA_SUBMENU_PAJ), entries)
-        end, 50)
+        }
+        AddCustomSubMenuItem(GetString(SI_PA_SUBMENU_PAJ), entries)
     end
 end
 
@@ -131,7 +127,10 @@ local function initHooksOnInventoryContextMenu()
                 if slotType == SLOT_TYPE_ITEM or slotType == SLOT_TYPE_BANK_ITEM then
                     local bagId, slotIndex = ZO_Inventory_GetBagAndIndex(inventorySlot)
                     local itemLink = GetItemLink(bagId, slotIndex)
-                    _addDynamicContextMenuEntries(itemLink, bagId, slotIndex)
+                    zo_callLater(function()
+                        _addDynamicContextMenuEntries(itemLink, bagId, slotIndex)
+                        ShowMenu()
+                    end, 50)
                 end
 
     --            if slotType == SLOT_TYPE_TRADING_HOUSE_ITEM_RESULT then
