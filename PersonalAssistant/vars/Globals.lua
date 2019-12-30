782247c955861e85c04767360fde7215632e2d2d
Klingo <klingo@klingonium.com>
2019-12-30 10:25:18 +0100

    prepare for Version 2.4.17

--- PersonalAssistant/vars/Globals.lua
+++ PersonalAssistant/vars/Globals.lua
@@ -47,7 +47,7 @@ PersonalAssistant.Constants = {
                 LOOT = 2,
                 REPAIR = 1,
             },
-            MINOR = 020416, -- update this every release!
+            MINOR = 020417, -- update this every release!
         },
     },
 
