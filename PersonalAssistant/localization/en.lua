0f8c2d56c3081e1429eb7a1a7c41d82882da0407
Klingo <klingo@klingonium.com>
2020-02-22 14:56:10 +0100

    add German translations

--- PersonalAssistant/localization/en.lua
+++ PersonalAssistant/localization/en.lua
@@ -122,15 +122,6 @@ local PAStrings = {
     SI_PA_REL_BANK_GREATERTHANOREQUAL = "BANK >=",
 
     -- -----------------------------------------------------------------------------------------------------------------
-    -- Text Operators --
-    SI_PA_REL_TEXT_OPERATOR0 = "-",
-    SI_PA_REL_TEXT_OPERATOR1 = "has exactly",
-    SI_PA_REL_TEXT_OPERATOR2 = "has less than", -- not used so far
-    SI_PA_REL_TEXT_OPERATOR3 = "hast at most",
-    SI_PA_REL_TEXT_OPERATOR4 = "has more than", -- not used so far
-    SI_PA_REL_TEXT_OPERATOR5 = "hast at least",
-
-    -- -----------------------------------------------------------------------------------------------------------------
     -- Operator Tooltip --
     SI_PA_REL_BACKPACK_EQUAL_T = "BACKPACK equals (==)",
     SI_PA_REL_BACKPACK_LESSTHAN_T = "BACKPACK less than (<)", -- not used so far
@@ -142,6 +133,15 @@ local PAStrings = {
     SI_PA_REL_BANK_LESSTHANOREQUAL_T = "BANK less than or equal to (<=)",
     SI_PA_REL_BANK_GREATERTHAN_T = "BANK greater than (>)", -- not used so far
     SI_PA_REL_BANK_GREATERTHANOREQUAL_T = "BANK greater than or equal to (>=)",
+
+    -- -----------------------------------------------------------------------------------------------------------------
+    -- Text Operators --
+    SI_PA_REL_TEXT_OPERATOR0 = "-",
+    SI_PA_REL_TEXT_OPERATOR1 = "has exactly",
+    SI_PA_REL_TEXT_OPERATOR2 = "has less than", -- not used so far
+    SI_PA_REL_TEXT_OPERATOR3 = "hast at most",
+    SI_PA_REL_TEXT_OPERATOR4 = "has more than", -- not used so far
+    SI_PA_REL_TEXT_OPERATOR5 = "hast at least",
 
     -- -----------------------------------------------------------------------------------------------------------------
     -- Stacking types --
