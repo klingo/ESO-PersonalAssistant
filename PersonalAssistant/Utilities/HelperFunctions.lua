bf66462759dd95021cec1619cfeea41ffb32c34c
Klingo <klingo@klingonium.com>
2021-05-01 17:24:51 +0200

    simplified function :)

--- PersonalAssistant/Utilities/HelperFunctions.lua
+++ PersonalAssistant/Utilities/HelperFunctions.lua
@@ -43,12 +43,7 @@ end
 ---@param key string|number the key that might in the table
 ---@return boolean whether the key exists in the table
 local function isKeyInTable(table, key)
-    for k in pairs(table) do
-        if k == key then
-            return true
-        end
-    end
-    return false
+    return table[key] ~= nil
 end
 
 
