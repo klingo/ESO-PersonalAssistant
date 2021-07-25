8816871bab165990a2bef062f10e8491ad48e2e7
Klingo <klingo@klingonium.com>
2021-07-25 11:55:58 +0200

    remove no longer needed comments

--- PersonalAssistant/PersonalAssistantJunk/PAJunk/PAJunkKeybindStrip.lua
+++ PersonalAssistant/PersonalAssistantJunk/PAJunk/PAJunkKeybindStrip.lua
@@ -72,7 +72,6 @@ local function _isDestroyItemEnabled()
             -- check for unknown motifs
             if IsItemLinkBook(itemLink) and not IsItemLinkBookKnown(itemLink) then return false end
         elseif specializedItemType == SPECIALIZED_ITEMTYPE_CONTAINER_STYLE_PAGE or specializedItemType == SPECIALIZED_ITEMTYPE_COLLECTIBLE_STYLE_PAGE then
-            -- APIVersion_100035: Need to check SPECIALIZED_ITEMTYPE_COLLECTIBLE_STYLE_PAGE in addition to SPECIALIZED_ITEMTYPE_CONTAINER_STYLE_PAGE
             -- check for unknown style page containers
             local containerCollectibleId = GetItemLinkContainerCollectibleId(itemLink)
             local isValidForPlayer = IsCollectibleValidForPlayer(containerCollectibleId)
